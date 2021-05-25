7f2c0be80f6697ce2d3fe2b8ab78b1eff647d386
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-05-25 15:38:38 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+function superbowlWin(record) {
+    function hasAWin(element, index, array) {
+        return (element.result === 'W')
+    } 
+    let superbowlWin = record.find(hasAWin)
+    console.log(superbowlWin)
+    
+    if (typeof superbowlWin === "object") { 
+        return superbowlWin.year
+   } else {
+       return undefined
+   }
+}
